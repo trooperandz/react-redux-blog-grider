ca46736622ad8b94a77588f7a6296e6630885a97
Matt Holland <mtholland10@gmail.com>
2017-05-29 14:47:33 -0500

    First test route component

--- src/index.js
+++ src/index.js
@@ -6,26 +6,18 @@ import { createStore, applyMiddleware } from 'redux';
 // Route component provides configuration to show a component based on the url route
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/PostsIndex';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Hello!</div> }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye!</div> }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container')
+);
