a7ef3d889dee689cea86de416868a90ae3a26d68
Matt Holland <mtholland10@gmail.com>
2017-06-04 22:37:46 -0500

    Complete delete post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
-import { FETCH_POST } from '../actions';
-import { DELETE_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch(action.type) {
+    case DELETE_POST:
+      // Look at state object, and if has a key of the post id, drop it and return a new object that doesn't contain that id
+      return _.omit(state, action.payload)
     case FETCH_POST:
       //const post = action.payload.data;
       // Take all existing posts, take all out of state object and put them into this new object that we are returning
